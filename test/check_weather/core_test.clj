255b58ed9d803b33479ebc2ca143d8551233ea63
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-10 14:45:10 +0100

    Adding first test

--- test/check_weather/core_test.clj
+++ test/check_weather/core_test.clj
@@ -1,7 +1,7 @@
 (ns check-weather.core-test
   (:require [clojure.test :refer :all]
-            [check-weather.core :refer :all]))
+            [check-weather.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(deftest testing-build-request-string
+  (is (= "http://api.weatherstack.com/current?access_key=928b850514d4dfdc3614fb973c27ebea&query=New York" (core/build-request-string "New York")))
+  )
