194f3a8fd906a686e6a464cf8d340298ddbbf374
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-12 22:23:43 +0100

    Adding namespace for functions connected with API

--- src/check_weather/core.clj
+++ src/check_weather/core.clj
@@ -1,80 +1,22 @@
 (ns check-weather.core
-  (:require [clj-http.client :as client]
-            [cheshire.core :as cheshire])
+  (:require [check-weather.weatherstack :as weatherstack])
   (:gen-class))
 
-(def access-key "928b850514d4dfdc3614fb973c27ebea")
-(def endpoint "http://api.weatherstack.com/")
-
-(defn log [& args]
-  (apply println args))
-
-(defn build-current-weather-request-str [query]
-  (str endpoint "current?access_key=" access-key "&query=" query))
-
-(defn send-request [request-string]
-  (-> (client/get request-string)
-      :body
-      (cheshire/parse-string true)))
-
-(defn get-current-weather
-  "If city found returns a map with current weather informations from Weatherstack API. 
-
-  A map in following format is returned:
-  {:request {:type \"City\"
-             :query \"New York, United States of America\"
-             :language \"en\"
-             :unit \"m\"}
-   :location {:name \"New York\"
-              :localtime \"2020-01-10 13:02\"
-              :timezone_id \"America/New_York\"
-              :region \"New York\"
-              :utc_offset \"-5.0\"
-              :lon \"-74.006\"
-              :lat \"40.714\"
-              :country \"United States of America\"
-              :localtime_epoch 1578661320}
-   :current {:weather_descriptions [\"Overcast\"]
-             :is_day \"yes\"
-             :pressure 1034
-             :uv_index 2
-             :wind_degree 0
-             :weather_code 122
-             :wind_speed 11
-             :cloudcover 100
-             :feelslike 4
-             :humidity 65
-             :observation_time \"06:02 PM\"
-             :weather_icons [\"https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0004_black_low_cloud.png\"]
-             :precip 0
-             :wind_dir \"N\"
-             :visibility 16
-             :temperature 7}}
-
-  Will throw an error if city is not found"
-  [query]
-  (let [response (send-request (build-current-weather-request-str query))]
-    (if (false? (get response :success))
-      (throw (ex-info "Please provide existing city" {:code (get-in response [:error :code])}))
-      response)))
-
-(defn get-location [current-weather]
+(defn current-weather->location [current-weather]
   (get-in current-weather [:request :query]))
 
-(defn get-temperature [current-weather]
+(defn current-weather->temperature [current-weather]
   (get-in current-weather [:current :temperature]))
 
-(defn get-pressure [current-weather]
+(defn current-weather->pressure [current-weather]
   (get-in current-weather [:current :pressure]))
 
-(defn get-windspeed [current-weather]
+(defn current-weather->windspeed [current-weather]
   (get-in current-weather [:current :wind_speed]))
 
 (defn get-weather [query]
-  (let [current-weather (get-current-weather query)]
-    (str "In " (get-location current-weather) " there is now " (get-temperature current-weather) " celcius, " (get-pressure current-weather) " hPa and wind is " (get-windspeed current-weather) " km/h."))
-  
-  )
+  (let [current-weather (weatherstack/get-current-weather query)]
+    (str "In " (current-weather->location current-weather) " there is now " (current-weather->temperature current-weather) " celcius, " (current-weather->pressure current-weather) " hPa and wind is " (current-weather->windspeed current-weather) " km/h.")))
 
 (defn -main
   [query]
---
+++ src/check_weather/weatherstack.clj
@@ -0,0 +1,56 @@
+(ns check-weather.weatherstack
+  (:require [clj-http.client :as client]
+            [cheshire.core :as cheshire]))
+
+
+(def access-key "928b850514d4dfdc3614fb973c27ebea")
+(def endpoint "http://api.weatherstack.com/")
+
+(defn build-current-weather-request-str [query]
+  (str endpoint "current?access_key=" access-key "&query=" query))
+
+(defn send-request [request-string]
+  (-> (client/get request-string)
+      :body
+      (cheshire/parse-string true)))
+
+(defn get-current-weather
+  "If city found returns a map with current weather informations from Weatherstack API. 
+
+  A map in following format is returned:
+  {:request {:type \"City\"
+             :query \"New York, United States of America\"
+             :language \"en\"
+             :unit \"m\"}
+   :location {:name \"New York\"
+              :localtime \"2020-01-10 13:02\"
+              :timezone_id \"America/New_York\"
+              :region \"New York\"
+              :utc_offset \"-5.0\"
+              :lon \"-74.006\"
+              :lat \"40.714\"
+              :country \"United States of America\"
+              :localtime_epoch 1578661320}
+   :current {:weather_descriptions [\"Overcast\"]
+             :is_day \"yes\"
+             :pressure 1034
+             :uv_index 2
+             :wind_degree 0
+             :weather_code 122
+             :wind_speed 11
+             :cloudcover 100
+             :feelslike 4
+             :humidity 65
+             :observation_time \"06:02 PM\"
+             :weather_icons [\"https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0004_black_low_cloud.png\"]
+             :precip 0
+             :wind_dir \"N\"
+             :visibility 16
+             :temperature 7}}
+
+  Will throw an error if city is not found"
+  [query]
+  (let [response (send-request (build-current-weather-request-str query))]
+    (if (false? (get response :success))
+      (throw (ex-info "Please provide existing city" {:code (get-in response [:error :code])}))
+      response)))
